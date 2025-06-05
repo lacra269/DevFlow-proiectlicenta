1888c2bde437a505a684e425fb86c4b3ad723850
lacra269 <lacramioara.bordeanu@student.uaic.ro>
2025-06-05 22:00:51 +0300

    Fix erori compilare

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "app/api/auth/[...nextauth]/route"; // Sau cale
+import { authOptions } from "lib/authOptions"; // Sau cale
 // Importă funcțiile Cloudinary din lib
 import {
     uploadToCloudinary,
