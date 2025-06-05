0549d0e41dc5713e8d8a9b4caa87d1c8722fd55c
lacra269 <lacramioara.bordeanu@student.uaic.ro>
2025-06-05 18:14:13 +0300

    Fix erorii

--- app/api/postss/route.js
+++ app/api/postss/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import prisma from '@/lib/prisma';
+import { authOptions } from '/app/api/auth/[...nextauth]/route';
+import prisma from '/lib/prisma';
 
 // POST: Creează o nouă postare
 export async function POST(request) {
